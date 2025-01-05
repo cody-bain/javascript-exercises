40edc97a885ad4449910648b1b3a5634dfd2e2ca
cody-bain <baincody@icloud.com>
2025-01-05 15:23:47 -0500

    complete js exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function (...args) {
+    let startIdx = args[0] < args[1] ? args[0] : args[1];
+    const endIdx = args[0] < args[1] ? args[1] : args[0];
+    let sum = 0;
+    if (startIdx > 0 && endIdx > 0 &&
+        !(startIdx % 1) && !(endIdx % 1) &&
+        typeof startIdx == "number" && typeof endIdx === "number") {
 
+        for (startIdx; startIdx <= endIdx; startIdx++) {
+            sum += startIdx;
+        }
+        return sum;
+    }
+    else {
+        return "ERROR";
+    }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
