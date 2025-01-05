fac2bc452b481a089528ca15f5dc353cc344487d
cody-bain <baincody@icloud.com>
2025-01-05 14:43:45 -0500

    complete js exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+    for (arg = 1; arg < arguments.length; arg++) {
+        for (i = 0; i < array.length; i++) {
+            if (array[i] === arguments[arg]) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    };
+    return array;
 };
 
 // Do not edit below this line
