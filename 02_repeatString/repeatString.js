37b7f7cd48c415407495b029ccfbb7f2a6671530
cody-bain <baincody@icloud.com>
2025-01-05 11:58:53 -0500

    complete javascript exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (repeatStr, num) {
+    let returnStr = '';
+    if (num < 0) { returnStr = 'ERROR'; }
+    else {
+        for (i = 0; i < num; i++) {
+            returnStr = returnStr.concat(repeatStr);
+        }
+    }
+    return returnStr;
 };
 
 // Do not edit below this line
