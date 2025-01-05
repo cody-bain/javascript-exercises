1c139d3f80c93a9329e39f273e350c242507ecec
cody-bain <baincody@icloud.com>
2025-01-05 16:34:22 -0500

    complete js exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    if (!(year % 100)) { return (!(year % 400)) ? true : false; }
+    else if (!(year % 4)) { return true; }
+    else { return false; }
 };
 
+leapYears(1997);
 // Do not edit below this line
 module.exports = leapYears;
