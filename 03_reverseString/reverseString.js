c0903624b0b1b2c408e87fadf38232ab01ca0535
cody-bain <baincody@icloud.com>
2025-01-05 12:53:40 -0500

    comple js exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    const indexedString = string.split('');
+    for (i = 0; i < string.length; i++) {
+        char = indexedString.shift();
+        indexedString.splice(string.length - (i + 1), 0, char);
+    }
+    return indexedString.join("");
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+console.log(reverseString("Reverse"));
