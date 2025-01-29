937d45c8ab9def0fc074e5a9fb9ddcbdf471eb7c
cody-bain <baincody@icloud.com>
2025-01-29 18:50:43 -0500

    complete math exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  array = array.reduce((total, currentNum) => {
+    return total + currentNum;
+  }, 0);
+  return array;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  array = array.reduce((total, currentNum) => {
+    return total * currentNum;
+  });
+  return array;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let total = 1;
+  for (let i = num; i > 0; i--) {
+    total = total * i;
+  }
+  return total;
 };
 
 // Do not edit below this line
