9d4f5934f3310af2184ac069d94f9f53279b5396
cody-bain <baincody@icloud.com>
2025-01-30 08:33:17 -0500

    complete object exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    currentYear = new Date().getFullYear();
+    people = people.map((person) => {
+        let personAge = person.yearOfDeath - person.yearOfBirth ? person.yearOfDeath - person.yearOfBirth : currentYear - person.yearOfBirth;
+        return {
+            name: person.name,
+            age: personAge
+        };
+    });
+    people.sort((a, b) => (a.age > b.age) ? 1 : -1);
 
+    return people[people.length - 1];
 };
 
 // Do not edit below this line
