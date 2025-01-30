b33110b6297b568903640dd7aaafb6ea4ed0324b
cody-bain <baincody@icloud.com>
2025-01-29 20:20:34 -0500

    complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    word = word.replace(/[^a-zA-Z0-9]/g, '');
+    const revWord = word.split('').reverse().join('');
+    console.log(`${revWord} and ${word}`);
+    return revWord === word;
 };
 
 // Do not edit below this line
