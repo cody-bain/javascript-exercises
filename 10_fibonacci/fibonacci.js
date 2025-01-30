8c22b89c5a56908f744725a397b0071c50cea4a4
cody-bain <baincody@icloud.com>
2025-01-29 21:00:16 -0500

    complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (fibIndex) {
+    let fibArray = [1];
 
+    if (fibIndex == 0) { return 0; }
+    if (fibIndex == 1) { return 1; }
+    if (fibIndex < 0) { return "OOPS"; }
+
+    for (let i = 1; i < fibIndex; i++) {
+        backTwo = fibArray[i - 2] ? fibArray[i - 2] : 0;
+        backOne = fibArray[i - 1] ? fibArray[i - 1] : 0;
+        fibArray.push(backTwo + backOne);
+    }
+
+    return fibArray[fibArray.length - 1];
 };
+
+// 1 1 2 3 5 8
 
 // Do not edit below this line
 module.exports = fibonacci;
